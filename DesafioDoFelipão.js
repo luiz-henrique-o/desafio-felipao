08aac886e00a821910cddcccab49918e2cf78eb3
luiz-henrique-o <luizhenriquedeoliveirasilva61@gmail.com>
2023-11-20 20:00:07 -0300

    projeto pronto!

---
+++ DesafioDoFelipão.js
@@ -0,0 +1,31 @@
+let nomeDoHeroi = "Jaxier Gonzalez" 
+let experienciaDoHeroi = 9000 
+experienciaDoHeroi++ 
+
+switch (experienciaDoHeroi) {
+    case 1000:
+    console.log("O herói de nome " + nomeDoHeroi + " esta no nivel ferro"  );
+    break;
+    case 1001: 
+    console.log("O herói de nome " + nomeDoHeroi + " esta no nivel bronze"  );
+    break;
+    case 2001:
+    console.log("O herói de nome " + nomeDoHeroi + " esta no nivel prata"  );
+    break;
+    case 6001:
+    console.log("O herói de nome " + nomeDoHeroi + " esta no nivel ouro"  );
+    break;
+    case 7001:
+    console.log("O herói de nome " + nomeDoHeroi + " esta no nivel platina"  );
+    break;
+    case 8001:
+    console.log("O herói de nome " + nomeDoHeroi + " esta no nivel ascendente"  );
+    break;
+    case 9001:
+    console.log("O herói de nome " + nomeDoHeroi + " esta no nivel imortal"  );
+    break; 
+    case 10001:
+    console.log("O herói de nome " + nomeDoHeroi + " esta no nivel radiante"  );
+    break;
+}
+
